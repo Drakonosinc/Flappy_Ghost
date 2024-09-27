edca76dbd4688b0b82e000f0118333dbb9638288
matias <mat20can@gmail.com>
2024-09-27 16:35:14 -0300

    version 5.0

--- Interface.py
+++ Interface.py
@@ -87,7 +87,7 @@ class interface(objects):
             self.screen.blit(self.font3.render("Sounds", True, "orange"),(35,self.height/2-250))
             self.button(self.screen,None,self.font2_5,self.sound_type["sound_menu"],self.sound_type["color_menu"],(35,self.height/2-150),self.GOLDEN,command=lambda:self.sound_on_off("sound_menu","color_menu","value_menu","Sound Menu",self.sound_back,True),sound_hover=self.sound_buttonletters,sound_touch=self.sound_touchletters)
             self.button(self.screen,None,self.font2_5,self.sound_type["sound_Game"],self.sound_type["color_game"],(35,self.height/2-100),self.GOLDEN,command=lambda:self.sound_on_off("sound_Game","color_game","value_game","Sound Game",self.sound_back_game),sound_hover=self.sound_buttonletters,sound_touch=self.sound_touchletters)
-            self.button(self.screen,0,self.font1,"←",self.WHITE,(35,self.height-100),self.GOLDEN,sound_hover=self.sound_buttonletters,sound_touch=self.sound_touchletters)
+            self.button(self.screen,4,self.font1,"←",self.WHITE,(35,self.height-100),self.GOLDEN,sound_hover=self.sound_buttonletters,sound_touch=self.sound_touchletters)
     def sound_on_off(self,sound:str,color=(0,0,0),value=True,type_sound="",sound_back=None,play=False):
         self.sound_type[value]=not self.sound_type[value]
         if self.sound_type[value]:
