65c8f087a9451aca9178b8268624f4813b165ccd
matias <mat20can@gmail.com>
2025-01-27 16:28:08 -0300

    version 10.0

--- Button.py
+++ Button.py
@@ -8,7 +8,7 @@ class Button:
         self.color=config.get("color",(255,255,255))
         self.color2=config.get("color2",(0,0,0))
         self.position=config.get("position",(0,0))
-        self.position2=config.get("position2",self.position)
+        self.position2=config.get("position2",None)
         self.pressed=config.get("pressed",True)
         self.detect_mouse=config.get("detect_mouse",True)
         self.type_button=config.get("type_button",0)
@@ -16,12 +16,10 @@ class Button:
         self.sound_touch=config.get("sound_touch",None)
         self.commands = [config.get(f"command{i}") for i in range(1,4)]
         self.button_states=config.get("button_states",{"sound_hover":True,"sound_touch":True})
-    def draw(self):
+    def draw(self,main=None):
         self.button=self.screen.blit(self.font.render(self.text,True,self.color),self.position) if self.type_button==0 else pygame.draw.polygon(self.screen, self.color, self.position)
-        self.update()
-    def update(self):
         if self.detect_mouse:self.mouse_collision(mouse_pos=pygame.mouse.get_pos())
-        if self.pressed:self.pressed_button(pressed_mouse=pygame.mouse.get_pressed())
+        if self.pressed:self.pressed_button(pressed_mouse=pygame.mouse.get_pressed(),main=main)
         else:return self.button
     def mouse_collision(self,mouse_pos):
         self.screen.blit(self.font.render(self.text,True,self.color2),self.position) if self.type_button==0 else pygame.draw.polygon(self.screen, self.color2, self.position2)
@@ -29,11 +27,11 @@ class Button:
             self.sound_hover.play(loops=0)
             self.button_states["sound_hover"]=False
         else:self.button_states["sound_hover"]=True
-    def pressed_button(self,pressed_mouse):
+    def pressed_button(self,pressed_mouse,main=None):
         if pressed_mouse[0] and self.button_states["sound_touch"]:
             self.sound_touch.play(loops=0)
             self.button_states["sound_touch"]=False
-            if self.main!=None:self.main=self.main
+            if main!=None:main=self.main
             self.execute_commands()
         else:self.button_states["sound_touch"]=True
     def execute_commands(self):
