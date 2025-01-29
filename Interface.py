82b81dbc3a0b0744375077e7d6a75f666df8564c
matias <mat20can@gmail.com>
2025-01-29 17:44:15 -0300

    version 10.0

--- Interface.py
+++ Interface.py
@@ -28,6 +28,7 @@ class interface(objects):
         self.buttons_main_menu()
         self.buttons_game_over()
         self.buttons_mode_game()
+        self.buttons_pausa()
     def draw_generation(self):
         if self.main==-1 and self.mode_game["Training AI"]:self.screen.blit(self.font3_5.render(f"Generation: {int(self.generation)}", True, "orange"),(35,0))
     def filt(self,width,height,number,color=(0,0,0),position=(0,0)):
@@ -76,10 +77,21 @@ class interface(objects):
         if self.main==3:
             self.filt(self.width,self.height,150,self.GRAY)
             self.screen.blit(self.font3.render("Pause", True, "orange"),(35,self.height/2-250))
-            self.button(self.screen,-1,self.font2_5,"Reset",self.WHITE,(35,self.height/2-150),self.GOLDEN,command=self.reset,sound_hover=self.sound_buttonletters,sound_touch=self.sound_touchletters)
             self.button(self.screen,4,self.font2_5,"Option",self.WHITE,(35,self.height/2-100),self.GOLDEN,command=self.reset,command2=self.check_sounds,sound_hover=self.sound_buttonletters,sound_touch=self.sound_touchletters)
             self.button(self.screen,0,self.font2_5,"Menu",self.WHITE,(35,self.height/2-50),self.GOLDEN,command=self.reset,command2=self.check_sounds,sound_hover=self.sound_buttonletters,sound_touch=self.sound_touchletters)
             self.button(self.screen,None,self.font2_5,"Exit",self.WHITE,(35,self.height/2),self.GOLDEN,command=self.close_game,sound_hover=self.sound_buttonletters,sound_touch=self.sound_exit)
+            self.execute_buttons(self.reset_button)
+    def buttons_pausa(self):
+        self.reset_button = Button({"screen": self.screen,
+                                    "font": self.font2_5,
+                                    "text": "Reset",
+                                    "color":self.WHITE,
+                                    "position": (35,self.height/2-150),
+                                    "color2": self.GOLDEN,
+                                    "sound_hover": self.sound_buttonletters,
+                                    "sound_touch": self.sound_touchletters,
+                                    "command1":self.reset,
+                                    "command2":lambda:setattr(self,'main',-1)})
     def menu_options(self):
         if self.main==4:
             self.screen.fill(self.BLACK)
