bb6b7f1e5f026260efb1d092c951d86df611e010
matias <mat20can@gmail.com>
2025-02-16 16:35:45 -0300

    version 10.0

--- Elements.py
+++ Elements.py
@@ -18,15 +18,16 @@ class objects():
             with open(os.path.join(self.config_path,"config.json"), 'r') as file:config = json.load(file)
             self.config_visuals = config["config_visuals"]
             self.config_keys = config["config_keys"]
-        except:self.config(alls=True)
+        except:self.config(alls=True),self.save_config()
     def config(self,visuals=False,keys=False,sounds=False,AI=False,alls=False):
         if visuals or alls:self.config_visuals={"background":["bg.png","bg_night.png"],"value_background":0,
                             "flyers":["flappy_ghost.png"],"value_flyers":0,
                             "tubes":["tube.png"],"value_tubes":0}
         if keys or alls:self.config_keys={"key_jump":K_SPACE,"Name_key1":"SPACE"}
         if sounds or alls:self.config_sounds={"sound_menu":True,"sound_game":True}
+        if AI or alls:self.config_AI={"generation_value":100,"population_value":20,"try_for_ai":3,"model_save":False}
     def save_config(self):
-        config = {"config_visuals": self.config_visuals,"config_keys": self.config_keys}
+        config = {"config_visuals": self.config_visuals,"config_keys": self.config_keys,"config_sounds":self.config_sounds,}
         with open(os.path.join(self.config_path,"config.json"), 'w') as file:json.dump(config, file, indent=4)
     def config_screen(self):
         self.width,self.height=800,600
--- Main.py
+++ Main.py
@@ -4,5 +4,5 @@ from Genetic_Algorithm import *
 if __name__=="__main__":
     best_model = genetic_algorithm_optimized(game:=Game(), input_size=len(game.get_state()), output_size=1)
     game.model = best_model
-    if game.mode_game["Training AI"]:save_model(game.model, torch.optim.Adam(game.model.parameters(), lr=0.001),game.model_path)
+    if game.config_AI["model_save"]:save_model(game.model, torch.optim.Adam(game.model.parameters(), lr=0.001),game.model_path)
 pygame.quit(),sys.exit()
