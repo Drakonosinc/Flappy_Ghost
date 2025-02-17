4a64aed86ae5fe5facf454d1384b3dc58e9794ce
matias <mat20can@gmail.com>
2025-02-17 15:33:20 -0300

    version 10.0

---
+++ Player.py
@@ -0,0 +1,22 @@
+from pygame import *
+class Player:
+    def __init__(self, x, y, width, height):
+        self.rect = Rect(x, y, width, height)
+        self.down_gravity:float = 0
+        self.isjumper:bool = False
+        self.reward:int = 0
+        self.scores:int=0
+        self.active:bool = True
+    def jump(self, jumper_value,sound):
+        if self.isjumper:
+            self.down_gravity = jumper_value
+            if sound!=None:sound.play()
+            self.isjumper = False
+    def fall(self, gravity):
+        self.down_gravity += gravity
+        self.rect.y += self.down_gravity
+    def reset(self, x, y):
+        self.rect.x, self.rect.y = x, y
+        self.down_gravity = 0
+        self.isjumper = False
+        self.scores = 0
