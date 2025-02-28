f2336a4870a69638c8978fc3fa2e9ad861b2fa6c
matias <mat20can@gmail.com>
2025-02-28 17:12:10 -0300

    version 10.0

--- src/Elements.py
+++ src/Elements.py
@@ -5,6 +5,7 @@ class objects():
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Flappy Bird")
+        self.base_dir = os.path.dirname(os.path.dirname(__file__))
         self.load_config()
         self.config_screen()
         self.load_images()
@@ -14,7 +15,7 @@ class objects():
         self.new_events()
     def load_config(self):
         try:
-            self.config_path = os.path.join(os.path.dirname(__file__), "Config")
+            self.config_path = os.path.join(self.base_dir, "Config")
             with open(os.path.join(self.config_path,"config.json"), 'r') as file:config = json.load(file)
             self.config_visuals = config["config_visuals"]
             self.config_keys = config["config_keys"]
@@ -29,7 +30,7 @@ class objects():
         if sounds or alls:self.config_sounds={"sound_menu":True,"sound_game":True}
         if AI or alls:self.config_AI={"generation_value":100,"population_value":20,"try_for_ai":3,"model_save":False}
     def save_config(self):
-        self.config_path = os.path.join(os.path.dirname(__file__), "Config")
+        self.config_path = os.path.join(self.base_dir, "Config")
         config = {"config_visuals": self.config_visuals,"config_keys": self.config_keys,"config_sounds":self.config_sounds,"config_AI":self.config_AI}
         with open(os.path.join(self.config_path,"config.json"), 'w') as file:json.dump(config, file, indent=4)
     def config_screen(self):
