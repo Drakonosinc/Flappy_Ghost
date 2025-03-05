59bc0516f33f3df6320bb5ba1aabb0accde5331f
matias <mat20can@gmail.com>
2025-03-05 14:03:07 -0300

    version 10.0

--- src/Interface.py
+++ src/Interface.py
@@ -165,9 +165,9 @@ class interface(objects):
         self.check_item(self.utils_keys,self.SKYBLUE,self.WHITE,"color",**{"key_jump":self.space_button})
     def event_keys(self,event):
         if self.key!=None and (self.utils_keys[self.key] and event.type==KEYDOWN):
-            self.config_keys[self.key]=event.key
-            self.config_keys[self.key_name]=event.unicode.upper()
-            self.check_item(self.config_keys,self.config_keys[self.key_name],self.WHITE,"text",**{self.key:self.button_key})
+            self.config.config_keys[self.key]=event.key
+            self.config.config_keys[self.key_name]=event.unicode.upper()
+            self.check_item(self.config.config_keys,self.config.config_keys[self.key_name],self.WHITE,"text",**{self.key:self.button_key})
             self.change_keys(self.key,self.key_name)
     def sounds_menu(self):
         if self.main==7:
