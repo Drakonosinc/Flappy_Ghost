be865ecc5f03b60a800a2a9f7e62934d1dbe80e6
matias <mat20can@gmail.com>
2025-03-06 16:01:26 -0300

    version 10.0

--- src/Interface.py
+++ src/Interface.py
@@ -23,7 +23,7 @@ class interface(objects):
         self.keys_menu()
         self.draw_generation()
     def draw_buttons(self):
-        self.button_factory_f2_5 = ButtonFactory({"screen": self.screen,"font": self.font2_5,"hover_color": self.GOLDEN,"sound_hover": self.sound_buttonletters,"sound_touch": self.sound_touchletters})
+        self.button_factory_f2_5 = ElementsFactory({"screen": self.screen,"font": self.font2_5,"hover_color": self.GOLDEN,"sound_hover": self.sound_buttonletters,"sound_touch": self.sound_touchletters})
         self.buttons_main_menu()
         self.buttons_game_over()
         self.buttons_mode_game()
