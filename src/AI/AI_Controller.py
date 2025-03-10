4d5cba8b58f7742a50e2f59a6fb8fa467c07a87e
matias <mat20can@gmail.com>
2025-03-10 16:26:59 -0300

    version 10.0

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -3,3 +3,10 @@ class AIHandler:
     def __init__(self, game):
         self.game = game
         self.models = []
+        
+    def get_state(self, player):
+        dist_to_tube_x = self.game.object2.x - player.rect.x
+        dist_to_tube_y = player.rect.y - self.game.object2.y
+        dist_to_tube_invert_y = player.rect.y - self.game.object3.y
+        dist_to_tube_to_tube_invert_y = self.game.object3.y - self.game.object2.y
+        
