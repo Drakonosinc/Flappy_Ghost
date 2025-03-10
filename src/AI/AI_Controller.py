26dcaae8360cbfc81342eaf084da7e422f87fef0
matias <mat20can@gmail.com>
2025-03-10 16:27:15 -0300

    version 10.0

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -9,4 +9,13 @@ class AIHandler:
         dist_to_tube_y = player.rect.y - self.game.object2.y
         dist_to_tube_invert_y = player.rect.y - self.game.object3.y
         dist_to_tube_to_tube_invert_y = self.game.object3.y - self.game.object2.y
-        
+        return np.array([
+            player.rect.x, player.rect.y,
+            self.game.object2.x, self.game.object2.y,
+            self.game.object3.x, self.game.object3.y,
+            self.game.object4.x, self.game.object4.y,
+            self.game.object5.x, self.game.object5.y,
+            dist_to_tube_x, dist_to_tube_y,
+            dist_to_tube_invert_y, dist_to_tube_to_tube_invert_y,
+            player.dy, self.game.speed_tubes
+        ])
