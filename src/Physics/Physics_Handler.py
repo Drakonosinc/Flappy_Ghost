7d76e7a9edf6e9d88c8e40febbdc9d50ff0bdab3
matias <mat20can@gmail.com>
2025-03-10 16:41:27 -0300

    version 10.0

--- src/Physics/Physics_Handler.py
+++ src/Physics/Physics_Handler.py
@@ -1,5 +1,5 @@
 class PhysicsHandler:
-    def __init__(self, gravity=0.25, jump_force=-12):
+    def __init__(self, gravity=0.25, jump_force=-5):
         self.gravity = gravity
         self.jump_force = jump_force
     def apply_gravity(self, player):
