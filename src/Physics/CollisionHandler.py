a4b229f4c2990b6cd46eab464da2b781c828e814
matias <mat20can@gmail.com>
2025-03-13 15:53:19 -0300

    version 10.0

--- src/Physics/CollisionHandler.py
+++ src/Physics/CollisionHandler.py
@@ -8,4 +8,13 @@ class CollisionHandler:
         player.reward += reward
         player.active = False
         self.game.restart()
+    def get_next_tubes(self, player, tubes):
+        sorted_tubes = sorted(tubes, key=lambda t: t.rect.x)
+        for i, tube in enumerate(sorted_tubes):
+            if tube.rect.x > player.rect.x:
+                current_tube = tube
+                next_tube1 = sorted_tubes[i + 1] if i + 1 < len(sorted_tubes) else None
+                next_tube2 = sorted_tubes[i + 2] if i + 2 < len(sorted_tubes) else None
+                return current_tube, next_tube1, next_tube2
+        return None, None, None
     
