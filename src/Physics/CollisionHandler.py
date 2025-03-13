8fd86a4bd88595029dbf9c909021839361879f46
matias <mat20can@gmail.com>
2025-03-13 15:52:47 -0300

    version 10.0

--- src/Physics/CollisionHandler.py
+++ src/Physics/CollisionHandler.py
@@ -3,4 +3,9 @@ class CollisionHandler:
         self.game = game
         self.sound_death = game.sound_death
     def check_collision(self, object1, object2):return object1.check_collision(object2)
+    def handle_collision(self, player, reward=-25):
+        self.sound_death.play(loops=0)
+        player.reward += reward
+        player.active = False
+        self.game.restart()
     
