580cd42b315ca2abf802cb7bc3fde53fc54a3c82
matias <mat20can@gmail.com>
2025-03-13 15:51:48 -0300

    version 10.0

--- src/Physics/CollisionHandler.py
+++ src/Physics/CollisionHandler.py
@@ -2,4 +2,5 @@ class CollisionHandler:
     def __init__(self, game):
         self.game = game
         self.sound_death = game.sound_death
+    def check_collision(self, object1, object2):return object1.check_collision(object2)
     
