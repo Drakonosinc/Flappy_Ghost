d099184a2af7904708b1f2eafc6ba3052e8f9962
matias <mat20can@gmail.com>
2025-03-17 15:57:33 -0300

    version 10.0

--- src/Entities/Tube.py
+++ src/Entities/Tube.py
@@ -1,13 +1,11 @@
 import pygame,os
 from Elements import *
 class Tube():
-    def __init__(self,x,y,angle,width_image,height_image,objects=objects()):
+    def __init__(self,x,y,angle,width,height,objects=objects()):
+        self.rect=pygame.Rect(x,y,width,height)
         self.objects=objects
         self.config_visuals=objects.config.config_visuals
-        self.load_tube(x,y,angle,width_image,height_image)
-    def load_tube(self,x,y,angle,width_image,height_image):
-        self.load_tube_image(angle,width_image,height_image)
-        self.rect=pygame.Rect(x,y,width_image,height_image)
+        self.load_tube_image(angle,width,height)
     def load_tube_image(self,angle,width_image,height_image):
         self.tube_image=pygame.image.load(os.path.join(self.objects.image_path,self.config_visuals["tubes"][self.config_visuals["value_tubes"]]))
         self.tube_image=pygame.transform.rotate(self.tube_image,angle)
