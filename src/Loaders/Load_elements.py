f49f4be74a52f90a8c1b71992d30ba214f8b8ad2
matias <mat20can@gmail.com>
2025-03-19 15:43:26 -0300

    version 10.0

--- src/Loaders/Load_elements.py
+++ src/Loaders/Load_elements.py
@@ -1,7 +1,7 @@
 import pygame,os
 from pygame.locals import *
 from AI.Genetic_Algorithm import *
-from Config_Loader import *
+from .Config_Loader import *
 class objects():
     def __init__(self):
         pygame.init()
