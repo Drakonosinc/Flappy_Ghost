ee3f4710826a3ee87dd91d8395b6acbb7cadd781
matias <mat20can@gmail.com>
2025-04-25 16:45:24 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -102,60 +102,7 @@ class PolygonButton(ElementBehavior):
         self.color=config.get("color",self.color)
         self.detect_mouse=config.get("detect_mouse",self.detect_mouse)
         self.pressed=config.get("pressed",self.pressed)
-class Input_text:
-    def __init__(self,config:dict):
-        self.screen=config["screen"]
-        self.font = config.get("font", pygame.font.Font(None, 25))
-        self.text = config.get("text","")
-        self.color=config.get("color",(0,0,0))
-        self.color_back=config.get("color_back",(255,255,255))
-        self.hover_color=config.get("hover_color",(255, 199, 51))
-        self.pressed_color=config.get("pressed_color",(135,206,235))
-        self.border_color=config.get("border_color",(127,127,127))
-        self.border=config.get("border",2)
-        self.commands = [config.get(f"command{i}") for i in range(1,4)]
-        self.position = config["position"]
-        self.sound_hover = config.get("sound_hover")
-        self.sound_touch = config.get("sound_touch")
-        self.pressed = config.get("pressed",True)
-        self.detect_mouse=config.get("detect_mouse",True)
-        self.states=config.get("states",{"detect_hover":True,"presses_touch":True,"active":False})
-        self.rect = pygame.Rect(*self.position)
-        self.new_events(time=config.get("time",500))
-    def new_events(self,time):
-        self.EVENT_NEW = pygame.USEREVENT + 1
-        pygame.time.set_timer(self.EVENT_NEW,time)
-    def reactivate_pressed(self,event):
-        if event.type==self.EVENT_NEW:self.states["presses_touch"]=True
-    def change_text(self,event):
-        if self.states["active"] and event.type==KEYDOWN:
-            if event.key == K_BACKSPACE:self.text=self.text[:-1]
-            else:self.text+=event.unicode
-    def draw(self):
-        pygame.draw.rect(self.screen,self.color_back,self.rect)
-        if self.detect_mouse:self.mouse_collision(pygame.mouse.get_pos())
-        if self.pressed:self.pressed_button(pygame.mouse.get_pressed(),pygame.mouse.get_pos())
-        input_player=pygame.draw.rect(self.screen,self.border_color,self.rect,self.border)
-        self.screen.blit(self.font.render(self.text, True, self.color), (input_player.x+5, input_player.y-2))
-    def mouse_collision(self,mouse_pos):
-        if self.rect.collidepoint(mouse_pos):
-            pygame.draw.rect(self.screen,self.hover_color,self.rect)
-            if self.states["detect_hover"]:
-                if self.sound_hover:self.sound_hover.play(loops=0)
-                self.states["detect_hover"]=False
-        else:self.states["detect_hover"]=True
-    def pressed_button(self,pressed_mouse,mouse_pos):
-        if pressed_mouse[0] and self.rect.collidepoint(mouse_pos) and self.states["presses_touch"]:
-            self.states["active"]=True
-            if self.sound_touch:self.sound_touch.play(loops=0)
-            self.states["presses_touch"]=False
-            self.execute_commands()
-        if pressed_mouse[0] and not self.rect.collidepoint(mouse_pos):self.states["active"],self.states["presses_touch"]=False,True
-        if self.states["active"]:pygame.draw.rect(self.screen,self.pressed_color,self.rect)
-    def execute_commands(self):
-        for command in self.commands:
-            if callable(command):command()
-    def show_player(self):return self.text
+
 class ScrollBar:
     def __init__(self,config:dict):
         self.screen=config["screen"]
