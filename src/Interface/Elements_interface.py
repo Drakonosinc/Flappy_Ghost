78d235bb83c8fb8bbc69ea5fc32992dea5351e42
matias <mat20can@gmail.com>
2025-04-25 16:44:10 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -83,7 +83,25 @@ class TextButton(Text,ElementBehavior):
         self.text=config.get("text",self.text)
         self.detect_mouse=config.get("detect_mouse",self.detect_mouse)
         self.pressed=config.get("pressed",self.pressed)
-
+class PolygonButton(ElementBehavior):
+    def __init__(self,config:dict):
+        super().__init__(config)
+        self.screen = config["screen"]
+        self.position = config["position"]
+        self.hover_position = config.get("hover_position",self.position)
+        self.color = config.get("color", (255, 255, 255))
+        self.hover_color = config.get("hover_color", (255, 199, 51))
+        self.detect_mouse=config.get("detect_mouse",True)
+        self.rect = pygame.draw.polygon(self.screen, self.color, self.position).copy()
+    def draw(self):
+        pygame.draw.polygon(self.screen, self.color, self.position)
+        if self.detect_mouse:self.mouse_collision(self.rect,pygame.mouse.get_pos())
+        if self.pressed:self.pressed_button(pygame.mouse.get_pressed(),pygame.mouse.get_pos())
+    def draw_hover_effect(self):return pygame.draw.polygon(self.screen, self.hover_color, self.hover_position)
+    def change_item(self,config:dict):
+        self.color=config.get("color",self.color)
+        self.detect_mouse=config.get("detect_mouse",self.detect_mouse)
+        self.pressed=config.get("pressed",self.pressed)
 class Input_text:
     def __init__(self,config:dict):
         self.screen=config["screen"]
