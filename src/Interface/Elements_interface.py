1db9a7e456ac4ce544884e763b6562642bef3d2c
matias <mat20can@gmail.com>
2025-04-25 16:40:16 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -29,6 +29,9 @@ class ElementBehavior:
         self.commands = [config.get(f"command{i}") for i in range(1,4)]
         self.new_events(time=config.get("time",500))
     def events(self, event):pass
+    def new_events(self,time):
+        self.EVENT_NEW = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.EVENT_NEW,time)
 class TextButton:
     def __init__(self,config:dict):
         self.screen = config["screen"]
