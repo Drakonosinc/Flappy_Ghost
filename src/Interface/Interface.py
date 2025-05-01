4d5f5789e46b5dd7d22b1613b47558729fae4d26
matias <mat20can@gmail.com>
2025-05-01 16:44:30 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -79,10 +79,10 @@ class interface(objects):
     def text_training_ai(self):
         if not hasattr(self, "text_in_training_ai"):
             self.text_C=self.button_factory_f2_5.create_Text({"text":(f"Config Training AI"),"position":(self.width/2+20,self.height/2-150),"detect_mouse":False})
-            self.text_G=self.button_factory_f5.create_Text({"text":(f"Generation Size\n{self.config.config_AI['generation_value']:^36}"),"position":(self.width/2+40,self.height/2-100),"detect_mouse":False})
-            self.text_P=self.button_factory_f5.create_Text({"text":(f"Population Size\n{self.config.config_AI['population_value']:^36}"),"position":(self.width/2+40,self.height/2-25),"detect_mouse":False})
-            self.text_A=self.button_factory_f5.create_Text({"text":(f"Attempts By AI\n{self.config.config_AI['try_for_ai']:^{39 if self.config.config_AI['try_for_ai']<10 else 36}}"),"position":(self.width/2+40,self.height/2+50),"detect_mouse":False})
-            self.text_S=self.button_factory_f5.create_Text({"text":(f"Save model"),"position":(self.width/2+40,self.height/2+125),"detect_mouse":False})
+            self.text_G=self.button_factory_f2_5.create_Text({"text":(f"Generation Size\n{self.config.config_AI['generation_value']:^36}"),"position":(self.width/2+40,self.height/2-100),"detect_mouse":False})
+            self.text_P=self.button_factory_f2_5.create_Text({"text":(f"Population Size\n{self.config.config_AI['population_value']:^36}"),"position":(self.width/2+40,self.height/2-25),"detect_mouse":False})
+            self.text_A=self.button_factory_f2_5.create_Text({"text":(f"Attempts By AI\n{self.config.config_AI['try_for_ai']:^{39 if self.config.config_AI['try_for_ai']<10 else 36}}"),"position":(self.width/2+40,self.height/2+50),"detect_mouse":False})
+            self.text_S=self.button_factory_f2_5.create_Text({"text":(f"Save model"),"position":(self.width/2+40,self.height/2+125),"detect_mouse":False})
             self.text_in_training_ai=[self.text_C,self.text_G,self.text_P,self.text_A,self.text_S]
         else:pass
     def buttons_config_AI(self):
