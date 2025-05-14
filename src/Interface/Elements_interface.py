7476df28746dd268ac6b364aa792ab0a0a2f6946
matias <mat20can@gmail.com>
2025-05-14 12:57:16 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -194,4 +194,6 @@ class Combo_Box(ElementBehavior):
         self.color = config.get("color", (255, 255, 255))
         self.rect = pygame.Rect(*self.position)
     def draw(self):pass
+    def draw_hover_effect(self):pass
+    def pressed_button(self,pressed_mouse,mouse_pos):pass
     def charge_elements(self, elements: list):pass
