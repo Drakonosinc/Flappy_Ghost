2e5f47f33e47f19ea4f7ff3ffd6f5d1f259531a9
matias <mat20can@gmail.com>
2025-07-11 14:47:04 -0300

    version 10.0

--- src/Interface/Menus/Game_Mode.py
+++ src/Interface/Menus/Game_Mode.py
@@ -3,3 +3,7 @@ class GameMode(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
         self.buttons = {}
+    def setup_buttons(self):
+        factory = self.interface.button_factory_f2_5
+    def render(self):
+        self.execute_buttons(*self.buttons.values())
