003f3c0ef496d2593c4e2a49ea179363b30dcac5
matias <mat20can@gmail.com>
2025-07-11 14:28:55 -0300

    version 10.0

--- src/Interface/Menus/Game_Over.py
+++ src/Interface/Menus/Game_Over.py
@@ -6,4 +6,6 @@ class GameOver(BaseMenu):
     def setup_buttons(self):
         factory = self.interface.button_factory_f2_5
     def render(self):
+        self.filt(self.WIDTH,self.HEIGHT,150,self.interface.RED)
+        self.screen.blit(self.interface.font4.render("Game Over", True, self.interface.BLACK),(120,self.HEIGHT/2-250))
         self.execute_buttons(*self.buttons.values())
--- src/Interface/Menus/Main_Menu.py
+++ src/Interface/Menus/Main_Menu.py
@@ -5,10 +5,10 @@ class MainMenu(BaseMenu):
         self.buttons = {}
     def setup_buttons(self):
         factory = self.interface.button_factory_f2_5
-        self.buttons['play'] = factory.create_TextButton({"text": "PLAY","position": (self.width/2-60, self.height/2-150),"command1":lambda:self.change_mains({"main":2})})
-        self.buttons['quit'] = factory.create_TextButton({"text": "QUIT","position": (self.width/2-60,self.height/2-115),"sound_touch": self.interface.sound_exit,"command1": self.interface.close_game})
-        self.buttons['options'] = factory.create_TextButton({"text": "OPTIONS","position": (self.width-180,self.height-50),"command1":lambda:self.change_mains({"main":4})})
+        self.buttons['play'] = factory.create_TextButton({"text": "PLAY","position": (self.WIDTH/2-60, self.HEIGHT/2-150),"command1":lambda:self.change_mains({"main":2})})
+        self.buttons['quit'] = factory.create_TextButton({"text": "QUIT","position": (self.WIDTH/2-60,self.HEIGHT/2-115),"sound_touch": self.interface.sound_exit,"command1": self.interface.close_game})
+        self.buttons['options'] = factory.create_TextButton({"text": "OPTIONS","position": (self.WIDTH-180,self.HEIGHT-50),"command1":lambda:self.change_mains({"main":4})})
     def render(self):
         self.screen.fill(self.interface.BLACK)
-        self.screen.blit(self.interface.font4.render("FLAPPY GHOST", True, "orange"),(35,self.height/2-250))
+        self.screen.blit(self.interface.font4.render("FLAPPY GHOST", True, "orange"),(35,self.HEIGHT/2-250))
         self.execute_buttons(*self.buttons.values())
