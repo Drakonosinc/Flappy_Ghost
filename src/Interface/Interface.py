4d4c83b898b43a5b5480487aa41f491239d998d0
matias <mat20can@gmail.com>
2025-07-13 17:28:56 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -48,15 +48,9 @@ class interface(load_elements,BaseMenu):
         self.save_model.change_item({"color":self.SKYBLUE if self.config.config_AI["model_save"] else self.RED,"text":"ON" if self.config.config_AI["model_save"] else "OFF"})
         self.scroll.update_elements([*self.buttons_in_config_ai,*self.text_in_training_ai])
     def text_training_ai(self):
-        if not hasattr(self, "text_in_training_ai"):
-            
-            
-            
-            self.text_in_training_ai=[self.text_C,self.text_G,self.text_P,self.text_A,self.text_S]
-        else:
-            self.text_G.change_item({"text": f"Generation Size\n{self.config.config_AI['generation_value']:^36}"})
-            self.text_P.change_item({"text": f"Population Size\n{self.config.config_AI['population_value']:^37}"})
-            self.text_A.change_item({"text": f"Attempts By AI\n{self.config.config_AI['try_for_ai']:^{39 if self.config.config_AI['try_for_ai']<10 else 36}}"})
+        self.text_G.change_item({"text": f"Generation Size\n{self.config.config_AI['generation_value']:^36}"})
+        self.text_P.change_item({"text": f"Population Size\n{self.config.config_AI['population_value']:^37}"})
+        self.text_A.change_item({"text": f"Attempts By AI\n{self.config.config_AI['try_for_ai']:^{39 if self.config.config_AI['try_for_ai']<10 else 36}}"})
     def buttons_config_AI(self):
         self.increase_generation = self.button_factory_f2_5.create_TextButton({"font":self.font3_5,"text": ">","position": (self.width-220,self.height/2-70),"command1":lambda:self.increase_decrease_variable(self.config.config_AI,'generation_value'),"command2":self.text_training_ai})
         self.decrease_generation = self.button_factory_f2_5.create_TextButton({"font":self.font3_5,"text": "<","position": (self.width-345,self.height/2-70),"command1":lambda:self.increase_decrease_variable(self.config.config_AI,'generation_value',True,-1),"command2":self.text_training_ai})
