ab05dd27c59519fc50ddaeb29c1a6bbabf0fd842
matias <mat20can@gmail.com>
2025-07-14 15:07:12 -0300

    version 10.0

--- src/Interface/Menus/Game_Mode.py
+++ src/Interface/Menus/Game_Mode.py
@@ -7,11 +7,16 @@ class GameMode(BaseMenu):
     def setup_buttons(self):
         factory = self.interface.button_factory_f2_5
         self.buttons['back'] = factory.create_TextButton({"font": self.interface.font1,"text": "←","position": (35,self.HEIGHT-100),"command1":lambda:self.change_mains({"main":0})})
-        self.buttons['continue'] = factory.create_TextButton({"font": self.interface.font1,"text": "→","position": (self.WIDTH-110,self.HEIGHT-100),"command1":lambda:self.type_game(False,True) if all(not mode for mode in self.interface.mode_game.values()) else None,"command2":lambda:(self.change_mains({"main":-1,"run":True,"command":self.interface.population}),self.interface.sound_back.stop(),self.interface.sound_back_game.play(loops=-1)if self.interface.sound_type["value_game"] else None)})
-        self.buttons['training_ai'] = factory.create_TextButton({"text": "Training AI","position": (35,self.HEIGHT/2-150),"command1":lambda:self.type_game(True),"command2":lambda:self.update_mode_buttons(self.buttons)})
-        self.buttons['player'] = factory.create_TextButton({"text": "Player","position": (35,self.HEIGHT/2-100),"command1":lambda:self.type_game(False,True),"command2":lambda:self.update_mode_buttons(self.buttons)})
-        self.buttons['ai'] = factory.create_TextButton({"text": "AI","position": (35,self.HEIGHT/2-50),"command1":lambda:self.type_game(False,False,True),"command2":lambda:self.update_mode_buttons(self.buttons)})
+        self.buttons['continue'] = factory.create_TextButton({"font": self.interface.font1,"text": "→","position": (self.WIDTH-110,self.HEIGHT-100),"command1":lambda:self._type_game(False,True) if all(not mode for mode in self.interface.mode_game.values()) else None,"command2":lambda:(self.change_mains({"main":-1,"run":True,"command":self.interface.population}),self.interface.sound_back.stop(),self.interface.sound_back_game.play(loops=-1)if self.interface.sound_type["value_game"] else None)})
+        self.buttons['training_ai'] = factory.create_TextButton({"text": "Training AI","position": (35,self.HEIGHT/2-150),"command1":lambda:self._type_game(True),"command2":lambda:self.update_mode_buttons(self.buttons)})
+        self.buttons['player'] = factory.create_TextButton({"text": "Player","position": (35,self.HEIGHT/2-100),"command1":lambda:self._type_game(False,True),"command2":lambda:self.update_mode_buttons(self.buttons)})
+        self.buttons['ai'] = factory.create_TextButton({"text": "AI","position": (35,self.HEIGHT/2-50),"command1":lambda:self._type_game(False,False,True),"command2":lambda:self.update_mode_buttons(self.buttons)})
         self._setup_training_ai_buttons(),self._setup_training_ai_texts()
+    def _type_game(self,mode_one=False,mode_two=False,mode_three=False):
+        self.mode_game["Training AI"]=mode_one
+        self.mode_game["Player"]=mode_two
+        if self.model_training!=None:self.mode_game["AI"]=mode_three
+        else:self.load_AI()
     def _setup_training_ai_buttons(self):
         factory = self.interface.button_factory_f2_5
     def _setup_training_ai_texts(self):
