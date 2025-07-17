d00094e2b816b3ce473865c03a7e5e67e57affc3
matias <mat20can@gmail.com>
2025-07-17 17:16:24 -0300

    version 10.0

--- src/Interface/Menus/__init__.py
+++ src/Interface/Menus/__init__.py
@@ -3,7 +3,7 @@ from .Main_Menu import MainMenu
 from .Game_Over import GameOver
 from .Game_Mode import GameMode
 from .Pause_Menu import Pause
-# from .Options_Menu import OptionsMenu
+from .Options_Menu import OptionsMenu
 # from .Visuals_Menu import VisualsMenu
 # from .Menu_Keys import KeysMenu
 # from .Menu_Sounds import SoundsMenu
