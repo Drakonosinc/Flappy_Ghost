0ec92cc94a29ee5a02190a296f9e49ec0be032d2
matias <mat20can@gmail.com>
2025-07-18 14:00:43 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -57,10 +57,6 @@ class interface(load_elements,BaseMenu):
         self.increase_tube_button = self.button_factory_f2_5.create_TextButton({"font": self.font3_5,"text": ">","position": (self.width/2+75,self.height/2),"command1":lambda:self.change_items("value_tubes","tubes",1)})
         self.save_visuals_button = self.button_factory_f2_5.create_TextButton({"text": "Save config","position": (self.width/2+80,self.height-85),"command1":self.config.save_config})
         self.default_visuals_button = self.button_factory_f2_5.create_TextButton({"text": "Default config","position": (self.width/2+50,self.height-50),"command1":lambda:self.config.config(visuals=True),"command2":self.load_visuals})
-    def items_visuals(self):
-        self.screen.blit(self.flappy_ghost,(self.players[0].rect.x-30,self.players[0].rect.y+50))
-        self.screen.blit(self.tubes[0].tube_image,(self.width/2-50,self.height/2))
-        self.screen.blit(self.tubes_invert[0].tube_image,(self.width/2+180,0))
     def change_items(self,item,background,number):
         self.config.config_visuals[item]=((self.config.config_visuals[item] + number) % len(self.config.config_visuals[background]))
         self.load_visuals()
