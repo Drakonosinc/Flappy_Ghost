482002721e7526bd6543c80322f853bbcae56e0a
matias <mat20can@gmail.com>
2025-07-22 17:00:43 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -52,7 +52,6 @@ class interface(load_elements,BaseMenu):
         self.sound_menu_button.change_item({"color":self.sound_type["color_menu"],"text":self.sound_type["sound_menu"]})
         self.sound_game_button.change_item({"color":self.sound_type["color_game"],"text":self.sound_type["sound_Game"]})
     def buttons_sounds(self):
-        
         self.sound_menu_button = self.button_factory_f2_5.create_TextButton({"text": self.sound_type["sound_menu"],"position": (35,self.height/2-150),"command1":lambda:self.sound_on_off("sound_menu","color_menu","value_menu","Sound Menu",self.sound_back,True),"command2":self.config.save_config})
         self.sound_game_button = self.button_factory_f2_5.create_TextButton({"text": self.sound_type["sound_Game"],"position": (35,self.height/2-100),"command1":lambda:self.sound_on_off("sound_Game","color_game","value_game","Sound Game",self.sound_back_game),"command2":self.config.save_config})
     def sound_on_off(self,sound:str,color,value=True,type_sound="",sound_back=None,play=False):
--- src/Interface/Menus/Menu_Sounds.py
+++ src/Interface/Menus/Menu_Sounds.py
@@ -6,6 +6,7 @@ class SoundsMenu(BaseMenu):
     def setup_buttons(self):
         factory = self.interface.button_factory_f2_5
         self.buttons['back'] = factory.create_TextButton({"font": self.interface.font1,"text": "←","position": (35,self.HEIGHT-100),"command1":lambda:self.change_mains({"main":4})})
+        
     def render(self):
         self.screen.fill(self.interface.BLACK)
         self.screen.blit(self.interface.font3.render("Sounds", True, "orange"),(35,self.HEIGHT/2-250))
