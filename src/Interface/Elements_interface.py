e444fc52edb95d3a5a48ac37970b55230562c7b3
matias <mat20can@gmail.com>
2025-08-09 15:54:38 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -249,9 +249,10 @@ class ComboBox(TextButton):
             case "right":return " >"
             case "left":return " <"
     def get_rect_dropdown(self):
+        def anim_height():pass
         match self.type_dropdown:
             case " V":
-                self.anim_height_dropdown += 1 if self.anim_height_dropdown<=self.dropdown[1] else 0
+                self.anim_height_dropdown += 1 if self.anim_height_dropdown<self.dropdown[1] else 0
                 return pygame.Rect(self.position[0], self.position[1] + self.font.get_height(), self.dropdown[0], self.anim_height_dropdown)
             case " Λ":return None
             case " >":return None
@@ -270,7 +271,9 @@ class ComboBox(TextButton):
         self.button_dropdown.change_item({"color": self.hover_dropdown})
         self.dropdown_rect = self.get_rect_dropdown()
         pygame.draw.rect(self.screen, self.hover_dropdown, self.dropdown_rect)
-        for button in self.option_buttons:button.draw()
+        pygame.draw.rect(self.screen, self.color, self.dropdown_rect, 2)
+        for button in self.option_buttons:
+            if button.rect.bottom<=self.dropdown_rect.bottom and button.rect.top>=self.dropdown_rect.top:button.draw()
         if hasattr(self, 'scroll'):self.scroll.draw()
     def charge_elements(self, options: list[str]):
         self.options = options
